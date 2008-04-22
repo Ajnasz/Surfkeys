7eb8b4c099893fcaea7ecc96e977fa6c1edfa35d
Lajos Koszti <ajnasz@ajnasz.hu>
2008-04-22 18:01:47 -0400

    update to 0.4.2 version update mkxpi.sh compatible with ff 3.0

--- src/mkxpi.sh
+++ src/mkxpi.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 echo Creating SurfKeys installation package
-echo Extension jar package surfkeys.jar
+echo Extension jar package chrome.jar
 cd chrome
-rm surfkeys.jar
-zip surfkeys.jar content/surfkeys/contents.rdf content/surfkeys/surfkeys.js content/surfkeys/surfkeyssettings.js content/surfkeys/surfkeys.xul content/surfkeys/surfkeys.css content/surfkeys/settings.xul locale/en-US/surfkeys/surfkeys.dtd locale/en-US/surfkeys/contents.rdf skin/classic/surfkeys/icon.png skin/classic/surfkeys/contents.rdf
+echo "delete jar";
+rm chrome.jar
 echo Install package surfkeys.xpi
+zip -r chrome.jar content/* -x CVS/*;
+zip -r chrome.jar locale/* -x CVS/*;
+zip -r chrome.jar skin/* -x CVS/*;
+
 cd ..
 rm ../downloads/surfkeys_$1.xpi
 echo Install package hah.xpi
-zip ../downloads/surfkeys_$1.xpi chrome/surfkeys.jar install.rdf
+rm surfkeys.xpi;
+zip surfkeys.xpi chrome.manifest install.rdf chrome/chrome.jar defaults/preferences/prefs.js -x CVS/*
+# zip ../downloads/surfkeys_$1.xpi chrome/chrome.jar install.rdf
 echo Done!
