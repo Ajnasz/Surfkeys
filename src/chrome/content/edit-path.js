59a9a0bd467374269e15e04a4ef4c72480689b91
Lajos Koszti <ajnasz@ajnasz.hu>
2009-01-18 19:17:48 +0100

    - change the namespace to SK.EditPath  - add some documentation

--- src/chrome/content/edit-path.js
+++ src/chrome/content/edit-path.js
@@ -1,46 +1,90 @@
-var EditPath = {
-  init: function() {
+/**
+ * @namespace SK
+ */
+SK.EditPath = {
+  /**
+   * Converts the current url to an object which contains the following properties:
+   *  original: the original url
+   *  domain: only the domain of the url
+   *  extended: the original url without it's last element
+   * @namespace SK.EditPath
+   * @method getURl
+   * @returns an object with the parsed url
+   * @type Object
+   */
+  getUrl: function() {
     var currloc = window.arguments[0];
-    var url = currloc.loc.replace(/^\w+:\/\//, '').split('/');
+    var url = currloc.loc.replace(/(^\w+:\/\/|\?.*$)/g, '').split('/');
     var values = {
       original: currloc.loc,
       domain: url[0],
       extended: url.slice(0, url.length).join('/'),
     };
+    return values;
+  },
+  /**
+   * Initializer function, which sets the input labels in the path editor window
+   */
+  init: function(event) {
+    var values = SK.EditPath.getUrl();
+    // get input elements
     var inputs = {
       original: document.getElementById('original'),
       extended: document.getElementById('extended'),
+      domain: document.getElementById('domain'),
       custom: document.getElementById('custom'),
       customPath: document.getElementById('custom-path'),
-      domain: document.getElementById('domain'),
       path: document.getElementById('path')
     };
+    // enable/disable the custom path input field
+    var customPath = {
+      enable: function() {
+        if(inputs.customPath.disabled) {
+          inputs.customPath.disabled = false;
+        }
+      },
+      disable: function() {
+        if(!inputs.customPath.disabled) {
+          inputs.customPath.disabled = true;
+        }
+      }
+    };
+
+
+    // sets the value of the input labels and inputs
     inputs.original.setAttribute('label', values.original);
     inputs.extended.setAttribute('label', values.extended);
     inputs.domain.setAttribute('label', values.domain);
     inputs.path.value = values.original;
     inputs.customPath.value = values.original;
+
+    // add event listeners
     inputs.original.addEventListener('click', function(event){
-      inputs.customPath.disabled = true;
+      customPath.disable();
       inputs.path.value = this.getAttribute('label');
     }, false);
     inputs.extended.addEventListener('click', function(event){
-      inputs.customPath.disabled = true;
+      customPath.disable();
       inputs.path.value = this.getAttribute('label');
     }, false);
     inputs.domain.addEventListener('click', function(event){
-      inputs.customPath.disabled = true;
+      customPath.disable();
       inputs.path.value = this.getAttribute('label');
     }, false);
     inputs.custom.addEventListener('click', function(event){
-      SKLog.log(inputs.customPath.id, inputs.custom.id);
-      inputs.customPath.disabled = false;
+      customPath.enable();
       inputs.path.value = inputs.customPath.value;
     }, false);
     inputs.customPath.addEventListener('keyup', function(event){
       inputs.path.value = this.value;
     }, false);
   },
+  /**
+   * saves the path value to the currloc variable
+   * @see surfkesy#SurfKeysAddNextPrev
+   * @returns true, to enable to close the window
+   * @type Boolean
+   */
   save: function() {
     var input = document.getElementById('path');
     var currloc = window.arguments[0];
